30b8d8f4035899652aa2e8fdb7b98f5e13bc88ba
Aiman Azmie <aimanaaw@gmail.com>
2019-09-17 21:21:11 +0000

    Committing eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🛑🛑🛑Assertion passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(x,y) {
+  if (x.length !== y.length) {
+    console.log("Not same lenght");
+    return false;
+  }
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] !== y[i]) {
+      return false;
+    } else if (x[i] === y[i] && (i === x.length -1)) {
+      return true;
+    }
+  }
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+
+eqArrays(["1", "2", "3"], ["1", "2", 3]);
