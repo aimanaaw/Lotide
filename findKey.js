76ed18da813e80f6d0b2e41ef487022512f300e0
Aiman Azmie <aimanaaw@gmail.com>
2019-09-19 16:53:03 -0400

    Finding key

---
+++ findKey.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKey = function (someObject, callback) {
+  for (let key in someObject) {
+    if (callback(someObject[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 },
+}, x => x.stars === 2), "noma");
