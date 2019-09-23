765d1af7462d11dad3c450e07fa4771fbc65ac6f
Aiman Azmie <aimanaaw@gmail.com>
2019-09-23 20:52:14 +0000

    Adding mocha tests to head tail and middle

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,19 @@
-const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+// const middle = require('../middle');
+// const assertArraysEqual = require('../assertArraysEqual');
 
 // Test Cases
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]); // => [3]
-assertArraysEqual(middle(([1, 2, 3, 4, 5, 6])), [3, 4]);
+// assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
+// assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]); // => [3]
+// assertArraysEqual(middle(([1, 2, 3, 4, 5, 6])), [3, 4]);
 
+const assert = require('chai').assert;
+const middle   = require('../middle');
 
-
+describe("#middle", () => {
+  it("returns the middle value if the array has an odd number of elements and the middle 2 values if the array has an even number of elements", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns the middle value if the array has an odd number of elements and the middle 2 values if the array has an even number of elements", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]),[3]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,25 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
+// const assertEqual = require('../assertEqual');
+// const tail = require('../tail');
 
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result[0], "Lighthouse");
+// assertEqual(result[1], "Labs");
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// assertEqual(words.length, 3);
+
+const assert = require('chai').assert;
+const tail   = require('../tail');
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+describe("#tail", () => {
+  it("returns the tail end of an array", () => {
+    assert.deepEqual((result[0]), "Lighthouse");
+  });
+  it("returns the tail end of an array", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.deepEqual(words.length, 3);
+  });
+});
