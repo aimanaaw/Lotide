23257eb3d9805d35e45b91e1dfdad4380abd0ee0
Aiman Azmie <aimanaaw@gmail.com>
2019-09-23 19:02:08 +0000

    Committing the refracted assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,5 @@
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`🛑🛑🛑Assertion passed: ${actual} === ${expected}`);
@@ -6,27 +8,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 }
 
-
-
-const eqArrays = function(x,y) {
-  if (x.length !== y.length) {
-    console.log("Not same lenght");
-    return false;
-  }
-  for (let i = 0; i < x.length; i++) {
-    if (x[i] !== y[i]) {
-      return false;
-    }; 
-    
-  }
-  return true;
-
-}
-
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-
-
-// eqArrays(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArraysEqual;
